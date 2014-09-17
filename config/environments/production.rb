bd24a2f87ede774895d7f23ed1cef75a67e5771b
Ilona Shub <ilonashub@Ilonas-MacBook-Pro.local>
2014-09-17 11:28:47 +0300

    Finish sign in

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ SampleApp::Application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security,
   # and use secure cookies.
   config.force_ssl = true
-  
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
